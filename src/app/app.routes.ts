6ca8141086247bbe1156a9a5774c0ba4f13977d9
asencio12 <mariluasencio5@gmail.com>
2026-09-09 17:09:23 -0600

    Proyecto Angular listo

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import{Home} from './pages/home/home';
+import{Contact} from './pages/contact/contact';
+
+export const routes: Routes = [
+    {path:'', redirectTo:'home', pathMatch:'full'},
+    {path: 'home',component:Home},
+    {path: 'contact',component:Contact},
+
+
+
+];
